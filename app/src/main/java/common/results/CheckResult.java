202a30121fc6b31e41108243cedb63481dbc7d10
AlanFierr0 <alan.fierro@ing.austral.edu.ar>
2023-11-16 16:47:03 -0300

    changed record to class

--- app/src/main/java/common/results/CheckResult.java
+++ app/src/main/java/common/results/CheckResult.java
@@ -1,4 +1,22 @@
 package common.results;
 
-public record CheckResult<T, R>(T successfulResult, R outputResult, String message) {
+public class CheckResult<T, R> {
+    private final T successfulResult;
+    private final R errorResult;
+    private final String message;
+
+    public CheckResult(T successfulResult, R errorResult,String message) {
+        this.successfulResult = successfulResult;
+        this.errorResult = errorResult;
+        this.message = message;
+    }
+
+    public T successfulResult() {
+        return successfulResult;
+    }
+
+
+    public Boolean outputResult(){
+        return (Boolean) errorResult;
+    }
 }

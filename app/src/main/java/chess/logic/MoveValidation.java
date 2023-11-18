96dccb519d389f36104c68d2381d0bf40b90873f
AlanFierr0 <85326611+AlanFierr0@users.noreply.github.com>
2023-11-18 16:52:22 -0300

    checkMate fixed

--- app/src/main/java/chess/logic/MoveValidation.java
+++ app/src/main/java/chess/logic/MoveValidation.java
@@ -5,11 +5,21 @@ import common.models.Coordinate;
 import common.moves.Move;
 import common.models.Piece;
 
+import java.util.Objects;
+
 public class MoveValidation {
-    public Boolean validateMove(Piece piece, Board board, Coordinate possibleMove, Coordinate initialSquare){
-        for (Move move : piece.getMovements()) {
-            if (move.checkMove(initialSquare, possibleMove, board, piece.getColor()).outputResult() && CommonRule.checkRule(board, piece, possibleMove)){
-                return true;
+    public Boolean validateMove(Piece piece, Board board, Coordinate possibleMove, Coordinate initialSquare) {
+        if (Objects.equals(board.getSquare(possibleMove).getPiece().getName(), "null")) {
+            for (Move move : piece.getMovements()) {
+                if (move.checkMove(initialSquare, possibleMove, board, piece.getColor()).outputResult() && CommonRule.checkRule(board, piece, possibleMove)) {
+                    return true;
+                }
+            }
+        } else {
+            for (Move move : piece.getEatMovements()) {
+                if (move.checkMove(initialSquare, possibleMove, board, piece.getColor()).outputResult() && CommonRule.checkRule(board, piece, possibleMove)) {
+                    return true;
+                }
             }
         }
         return false;

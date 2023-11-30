69d020c964722990b2b225cad6d2096b3a1dbcaa
AlanFierr0 <alan.fierro@ing.austral.edu.ar>
2023-11-30 14:47:02 -0300

    fix

--- app/src/main/java/checkers/logic/CheckForWinByNoMoreMoves.java
+++ app/src/main/java/checkers/logic/CheckForWinByNoMoreMoves.java
@@ -8,7 +8,7 @@ import common.models.Piece;
 public class CheckForWinByNoMoreMoves {
     PossibleMovements possibleMovements = new PossibleMovements();
     public Boolean check(Board board){
-        for (Piece p : board.getPieces()) {
+        for (Piece p : board.getCurrentPieces()) {
             if (!possibleMovements.getPossibleMovements(board, p, board.getSquareOfPiece(p).successfulResult().get()).isEmpty()) {
                 return false;
             }

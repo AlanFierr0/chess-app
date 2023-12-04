bf074ff2d31103a30be3ce27c9c25d4118cc5073
AlanFierr0 <85326611+AlanFierr0@users.noreply.github.com>
2023-12-04 17:38:08 -0300

    refactor for and if attached together

--- app/src/main/java/chess/logic/classicGame/CheckForCheckMate.java
+++ app/src/main/java/chess/logic/classicGame/CheckForCheckMate.java
@@ -16,23 +16,35 @@ public class CheckForCheckMate {
         public Boolean check(Board board, SideColor color, List<Piece> pieces){
             SideColor oppositeColor = color == SideColor.White ? SideColor.Black : SideColor.White;
             for (Piece piece : pieces) {
-                if (piece.getColor() == oppositeColor) {
-                    if (board.getCoordOfPiece(piece).successfulResult().isEmpty()) {
-                        continue;
-                    }
-                    Coordinate initialSquare = board.getCoordOfPiece(piece).successfulResult().get();
-                    List<Coordinate> possibleMoves = possibleMovements.getPossibleMovements(board,piece, initialSquare);
-                    for (Coordinate possibleMove : possibleMoves) {
-                        if(!moveValidation.validateMove(piece, board,possibleMove, initialSquare)){
-                            continue;
-                        }
-                        Board newBoard = board.positionPiece(piece, possibleMove);
-                        if(!checkForCheck.check(newBoard,oppositeColor, piece, possibleMove)){
-                            return false;
-                        }
-                    }
-                }
+                if (checkEachValidPossibleMove(board, piece, oppositeColor))
+                    return false;
             }
             return true;
         }
+
+    private boolean theresNoInitialPiece(Board board, Piece piece) {
+        return board.getCoordOfPiece(piece).successfulResult().isEmpty();
+    }
+
+    private boolean checkEachValidPossibleMove(Board board, Piece piece, SideColor oppositeColor) {
+        if (piece.getColor() == oppositeColor && !theresNoInitialPiece(board, piece)) {
+            Coordinate initialSquare = board.getCoordOfPiece(piece).successfulResult().get();
+            List<Coordinate> possibleMoves = possibleMovements.getPossibleMovements(board, piece, initialSquare);
+            return checkEachValidPossibleMove(board, piece, possibleMoves, initialSquare, oppositeColor);
+        }
+        return false;
+    }
+
+    private boolean checkEachValidPossibleMove(Board board, Piece piece, List<Coordinate> possibleMoves, Coordinate initialSquare, SideColor oppositeColor) {
+        for (Coordinate possibleMove : possibleMoves) {
+            if(!moveValidation.validateMove(piece, board,possibleMove, initialSquare)){
+                continue;
+            }
+            Board newBoard = board.positionPiece(piece, possibleMove);
+            if(!checkForCheck.check(newBoard, oppositeColor, piece, possibleMove)){
+                return true;
+            }
+        }
+        return false;
+    }
 }
